beb2ca5559caa3218143c895ef76d86160d073f1
reyangurjar <reyanyashgurjar@gmail.com>
2023-01-02 12:37:04 +0000

    remove cacheTime and persist , s-maxage to max-age

--- pages/api/getEpisodeInfo/[id].js
+++ pages/api/getEpisodeInfo/[id].js
@@ -57,15 +57,17 @@ export default async function handler(req, res) {
         episodeInfo.dubTotalEpisodes =dubAnimeInfo.totalEp
         if(dubAnimeInfo.status == 'Completed'){
             // To cache data for later if the anime is not releasing
-            // It will not work in development mode
-            res.setHeader('Cache-Control', 's-maxage=86400');
+            console.log("setting cache for completed dub")
+            // console.log(res.setHeader())
+            res.setHeader('Cache-Control', 'max-age=86400');
         }
     }
+    // If there is no dub episodes but sub is complete
     if(!episodeInfo.isDub){
         if(subAnimeInfo.status == 'Completed'){
             // To cache data for later if the anime is not releasing
-            // It will not work in development mode
-            res.setHeader('Cache-Control','s-maxage=86400');
+            console.log("setting cache for completed sub")
+            res.setHeader('Cache-Control','max-age=86400');
         }
     }
     res.send(episodeInfo)
